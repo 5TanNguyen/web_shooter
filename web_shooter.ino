b87f888a279c3c5cefc7f33c0c7ca5e25eaf74d6
5TanNguyen <126997329+5TanNguyen@users.noreply.github.com>
2024-11-24 14:14:42 +0700

    second commit

--- web_shooter.ino
+++ web_shooter.ino
@@ -1,20 +1,38 @@
+#define D13 13
+#define D4  4
+#define D9  9
+
 // cam bien van tay
 int Touch_Sensor = A5;
-int LED = 13;
-int Relay = 4;
-
+int LED = D13;
+int Relay = D4;
 int condition = 0;
 int state = 0;
+
+// cam bien hong ngoai
+int IRSensor = A2;
+int IRLed = D9;
 
 void setup() {
   // cam bien van tay
   pinMode(Touch_Sensor, INPUT);
   pinMode(LED, OUTPUT);
   pinMode(Relay, OUTPUT);
+
+  // camm bien hong ngoai
+  pinMode(IRSensor, INPUT);
+  pinMode(IRLed, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int statusSensor = analogRead(IRSensor);
+  if(statusSensor > 1000){
+    digitalWrite(IRLed, LOW);
+  }else{
+    digitalWrite(IRLed, HIGH);
+  }
+
 
  // cam bien van tay
  condition = digitalRead(A5);
@@ -22,8 +40,10 @@ void loop() {
     delay(300); 
     if(condition == 1){
       state = ~state;
-      digitalWrite( LED, state);
-      digitalWrite(Relay, state);
+      // digitalWrite( LED, state);
+      digitalWrite( LED, 1);
+      // digitalWrite(Relay, state);
+      digitalWrite(Relay, 1);
     }
   }
 }
